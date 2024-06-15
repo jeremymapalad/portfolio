f2d21510a56ee7faebb7388435eba1a7c40a60a2
Jeremy Mapalad <jeremymapalad@gmail.com>
2024-06-15 23:38:11 +0800

    Updates

---
+++ registerSW.js
@@ -0,0 +1,1 @@
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/jeremymapalad-portfolio/sw.js', { scope: '/jeremymapalad-portfolio/' })})}
